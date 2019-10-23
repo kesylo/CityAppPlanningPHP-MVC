786ec15a57e897f69a7f2be3426aba4a21e46fdc
kesylo <keming.loic@gmail.com>
2019-10-23 06:51:05 +0200

    added header and footer includes

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,6 +1,12 @@
+<!--add header-->
+<?php require APPROOT . '/views/includes/header.php'; ?>
+
 <h1>
     <!--receive data array and display-->
     <?php
         echo $data['title'];
     ?>
 </h1>
+
+<!--add footer-->
+<?php require APPROOT . '/views/includes/footer.php'; ?>
